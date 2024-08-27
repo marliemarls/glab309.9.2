dd0a1c4dcebfa5bb83df0ef6e9f11799f044a030
Marlie Martinez <marlieahue@gmail.com>
2024-08-27 14:20:31 -0400

    first commit

---
+++ src/main/java/com/perscholas/glab30992/model/FileUploadMode.java
@@ -0,0 +1,10 @@
+package com.perscholas.glab30992.model;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileUploadMode {
+    private String uploadedFile;
+}
